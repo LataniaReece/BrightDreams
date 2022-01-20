d0aef22f64a6f4164fbbe7a0de7baf3515f90c39
YOUR_USERNAME <latania.reece@outlook.com>
2022-01-20 13:06:39 -0500

    initial bright dreams commit updated from bright fields and added animation

---
+++ main.js
@@ -0,0 +1,52 @@
+const smallNavButtonEl = document.querySelector('.small-nav-button');
+const smallNavButtonIconEl = document.querySelector('.small-nav-button i');
+const navLinksEl = document.querySelector('.nav-links');
+const allNavLinks = document.querySelectorAll('.nav-links .nav-link');
+
+const navSlide = () => {
+  smallNavButtonEl.addEventListener('click', () => {
+    // Toggle Nav
+    navLinksEl.classList.toggle('show');
+    // Animate Links
+    allNavLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+
+    if (navLinksEl.classList.contains('show')) {
+      const newLogoEL = document.createElement('h4');
+      newLogoEL.innerHTML =
+        ' <img src="./assets/images/flower.png" alt="" />Bright<span>Dreams</span>Inc';
+      newLogoEL.classList.add('logo');
+      navLinksEl.insertAdjacentElement('afterbegin', newLogoEL);
+    }
+
+    smallNavButtonIconEl.classList.toggle('open');
+    // Change caret sign
+    if (smallNavButtonIconEl.classList.contains('open')) {
+      document.querySelector('.navbar .logo').style.opacity = '0';
+      smallNavButtonIconEl.style.animation = 'caretRotateUp 0.5s ease forwards';
+      document.getElementById('overlay').style.opacity = '1';
+    } else {
+      document.querySelector('.navbar .logo').style.opacity = '1';
+      smallNavButtonIconEl.style.animation =
+        'caretRotateDown 0.5s ease forwards';
+      document.getElementById('overlay').style.opacity = '0';
+      smallNavButtonEl.style.pointerEvents = 'none';
+
+      if (document.querySelector('.nav-links h4')) {
+        setTimeout(function () {
+          document.querySelector('.nav-links h4').remove();
+          smallNavButtonEl.style.pointerEvents = 'auto';
+        }, 1000);
+      }
+    }
+  });
+};
+
+navSlide();
